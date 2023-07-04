216de59b40ddf20b3fdafe2cd02f35a661023ce2
Elvin Li <li.elvin739@gmail.com>
2023-07-03 19:31:40 -0700

    Creating base
    Flask backend, HTML/CSS/JS frontend.

---
+++ AlgoNavigator/website/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "random ass key" # We need to add a secret key to run our server for some reason
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # Creates key with database as value
+    db.init_app(app) 
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix = '/')
+    app.register_blueprint(auth, url_prefix = '/')
+
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login' # Shows the page
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id)) # .get method looks for primary key, so we dont have to use id='' as a parameter
+
+    return app
