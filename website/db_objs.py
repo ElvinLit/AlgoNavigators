7c1828714c686fdb0e8f0b707c84d9b5264b105e
Elvin Li <li.elvin739@gmail.com>
2023-08-22 19:55:04 -0500

    Added new object for final travel plan

--- website/db_objs.py
+++ website/db_objs.py
@@ -11,23 +11,31 @@ class Note(db.Model):
     data = db.Column(db.String(100000))
     # activities = db.Column(JSON)
     db.Column(db.DateTime(timezone=True), default=func.now)
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # Matches with class User's id
+
 
 class UserMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class Activities(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(10000))
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class Restaurants(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     restaurant = db.Column(db.String(10000))
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class Flights(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -39,6 +47,7 @@ class Flights(db.Model):
     first_arrival_time = db.Column(db.String(10000))
     first_duration = db.Column(db.String(10000))
     first_link = db.Column(db.String(10000))
+    departure_date = db.Column(db.String(10000))
     second_cost = db.Column(db.Integer)
     second_airline = db.Column(db.String(10000))
     second_departure_airport = db.Column(db.String(10000))
@@ -47,14 +56,55 @@ class Flights(db.Model):
     second_arrival_time = db.Column(db.String(10000))
     second_duration = db.Column(db.String(10000))
     second_link = db.Column(db.String(10000))
+    return_date = db.Column(db.String(10000))
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class Hotels(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer)
     location = db.Column(db.String(10000))
     rating = db.Column(db.String(10000))
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class FinalPlan(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    # Flight Details
+    first_flight_cost = db.Column(db.Integer)
+    first_departure_airport = db.Column(db.String(10000))
+    first_arrival_airport = db.Column(db.String(10000))
+    first_departure_time = db.Column(db.String(10000))
+    first_arrival_time = db.Column(db.String(10000))
+    first_duration = db.Column(db.String(10000))
+    first_flight_link = db.Column(db.String(10000))
+    departure_date = db.Column(db.String(10000))
+    second_cost = db.Column(db.Integer)
+    second_airline = db.Column(db.String(10000))
+    second_departure_airport = db.Column(db.String(10000))
+    second_arrival_airport = db.Column(db.String(10000))
+    second_departure_time = db.Column(db.String(10000))
+    second_arrival_time = db.Column(db.String(10000))
+    second_duration = db.Column(db.String(10000))
+    second_flight_link = db.Column(db.String(10000))
+    return_date = db.Column(db.String(10000))    
+    
+    # Activities
+    activities = db.Column(db.String(10000))
+
+    # Hotel Information
+    hotel_price = db.Column(db.Integer)
+    hotel_location = db.Column(db.String(10000))
+    hotel_rating = db.Column(db.String(10000))
+
+    # Restaurants
+    restaurant = db.Column(db.String(10000))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -66,4 +116,5 @@ class User(db.Model, UserMixin):
     flight_information = db.relationship('Flights')
     restaurant_suggestions = db.relationship('Restaurants')
     activities_suggestions = db.relationship('Activities')
+    final_plan = db.relationship('FinalPlan')
 
